c427207d4f0c7df90da893c2d30f143593c7a443
Brandon Castro <C1brandon626@gmail.com>
2024-01-08 15:50:49 -0800

    Added folders 6 and 7, 6 is done!

---
+++ 6/src/shaders/vertex.glsl
@@ -0,0 +1,43 @@
+uniform float uSize;
+uniform float uTime; 
+
+attribute float aScale;
+attribute vec3 aRandom;
+
+varying vec3 vColor; 
+
+void main()
+{
+    /**
+        * Position
+        */
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Spin 
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime * 0.02;
+    angle += angleOffset;
+
+    modelPosition.x = cos(angle) * distanceToCenter; 
+    modelPosition.z = sin(angle) / distanceToCenter;
+    modelPosition.y = tan(angle) / distanceToCenter;
+
+    modelPosition.x += aRandom.x; 
+    modelPosition.y += aRandom.y; 
+    modelPosition.z += aRandom.z * uTime * 0.02;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    /**
+        * Size
+        */
+
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= (1.0 / -viewPosition.z);
+
+    vColor = color;
+}
